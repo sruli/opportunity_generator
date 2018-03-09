d3339b5f8affe50ff1db31e0ae08139a961c5b84
Sruli Rapps <srulirapps@gmail.com>
2018-03-09 16:05:50 +0200

    add redux dev tools

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import Main from './scenes/main';
@@ -8,7 +8,15 @@ import sagas from './sagas';
 import './App.css';
 
 const sagaMiddleware = createSagaMiddleware();
-export const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+export const store = createStore(reducer, /* preloadedState, */ enhancer);
 
 sagas.forEach(saga => sagaMiddleware.run(saga));
 
